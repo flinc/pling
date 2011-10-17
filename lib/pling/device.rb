4fe063afb21d63be56cc7d1c5ee5716a32c7cf5d
benedikt <benedikt@synatic.net>
2011-10-17 15:28:19 +0200

    Implements basic behavior of Pling::Device.
    Added methods:
    
    - Pling::Device.new
    - Pling::Device#identifier
    - Pling::Device#identifier=
    - Pling::Device#type
    - Pling::Device#type=
    - Pling::Device#valid?
    - Pling::Device#deliver
    - Pling::Device#to_pling_device

--- lib/pling/device.rb
+++ lib/pling/device.rb
@@ -1,4 +1,63 @@
 module Pling
   class Device
+
+    attr_reader :identifier, :type
+
+    ##
+    # Creates a new Pling::Device instance with the given identifier and type
+    #
+    # @param [Hash] attributes
+    # @option attributes [#to_s] :identifier
+    # @option attributes [#to_sym] :type
+    def initialize(attributes = {})
+      attributes.each_pair do |key, value|
+        method = "#{key}="
+        send(method, value) if respond_to?(method)
+      end
+    end
+
+    ##
+    # Sets the identifier to the given value.
+    #
+    # @param [#to_s] identifier
+    def identifier=(identifier)
+      identifier &&= identifier.to_s
+      @identifier = identifier
+    end
+
+    ##
+    # Sets the type to the given value.
+    #
+    # @param [#to_sym] type
+    def type=(type)
+      type &&= type.to_sym
+      @type = type
+    end
+
+    ##
+    # A device is valid if it has a type and an identifier.
+    #
+    # @return [Boolean]
+    def valid?
+      !!(type && identifier)
+    end
+
+    ##
+    # Delivers the given message using an appropriate gateway.
+    #
+    # @param [#to_pling_message] message
+    def deliver(message)
+      raise "The given object #{message.inspect} does not implement #to_pling_message" unless message.respond_to?(:to_pling_message)
+      message &&= message.to_pling_message
+    end
+
+    ##
+    # Returns the object itself as it is already a Pling::Device.
+    #
+    # @return [Pling::Device]
+    def to_pling_device
+      self
+    end
+
   end
 end
--- spec/pling/device_spec.rb
+++ spec/pling/device_spec.rb
@@ -1,5 +1,71 @@
 require 'spec_helper'
 
 describe Pling::Device do
+
+  context 'when created with no arguments' do
+    it 'should require an argument' do
+      expect { Pling::Device.new() }.to_not raise_error ArgumentError
+    end
   
+    specify { Pling::Device.new().should_not be_valid }
+  end
+  
+  context 'when created with an empty hash' do
+    it 'should accept a hash of attributes' do
+      expect { Pling::Device.new({}) }.to_not raise_error
+    end
+
+    specify { Pling::Device.new({}).should_not be_valid }
+  end
+
+  context 'when created with an hash of valid attributes' do
+    subject { Pling::Device.new(:identifier => 'XXXX', :type => 'android') }
+
+    its(:identifier) { should eq('XXXX') }
+    its(:type) { should eq(:android) }
+
+    it { should be_valid }
+  end
+
+  context 'when created with an hash of invalid attributes' do
+    it 'should ignore the invalid paramters' do
+      expect { Pling::Device.new({ :random_param => true }) }.to_not raise_error
+    end
+  end
+
+  describe '#to_pling_device' do
+    it 'should return self' do
+      subject.to_pling_device.should be === subject
+    end
+  end
+
+  describe '#identifier=' do
+    it 'should call #to_s on the given identifier' do
+      subject.identifier = stub(:to_s => 'XXXX')
+      subject.identifier.should eq('XXXX')
+    end
+  end
+
+  describe '#type=' do
+    it 'should call #to_sym on the given type' do
+      subject.type = 'android'
+      subject.type.should eq(:android)
+    end
+  end
+
+  it { should respond_to :deliver }
+
+  describe '#deliver' do
+    subject { Pling::Device.new(:identifier => 'XXXX', :type => 'android') }
+
+    it 'should require a message as parameter' do
+      expect { subject.deliver }.to raise_error ArgumentError
+    end
+
+    it 'should call #to_pling_message on the given message' do
+      message = mock()
+      message.should_receive(:to_pling_message).and_return(Pling::Message.new)
+      subject.deliver(message)
+    end
+  end
 end
