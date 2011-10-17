7875ff9cca0571a931bcfbfba2df552a318a1557
benedikt <benedikt@synatic.net>
2011-10-17 16:16:08 +0200

    Added methods to Pling::Message
    - Pling::Message.new
    - Pling::Message#content
    - Pling::Message#content=
    - Pling::Message#valid?
    - Pling::Message#to_pling_message

--- lib/pling/message.rb
+++ lib/pling/message.rb
@@ -1,4 +1,56 @@
 module Pling
   class Message
+    ##
+    # The message content
+    #
+    # @overload content
+    # @overload content=(content)
+    #   @param [#to_s] content
+    attr_reader :content
+
+    def content=(content)
+      content &&= content.to_s
+      @content = content
+    end
+
+    ##
+    # Creates a new Pling::Message instance with the given content
+    #
+    # @overload initialize(content)
+    #   @param [#to_s] content
+    # @overload initialize(attributes)
+    #   @param [Hash] attributes
+    #   @option attributes [#to_s] :content
+    def initialize(*args)
+      attributes = case param = args.shift
+        when String
+          (args.last || {}).merge(:content => param)
+        when Hash
+          param
+        else 
+          {}
+      end
+
+      attributes.each_pair do |key, value|
+        method = "#{key}="
+        send(method, value) if respond_to?(method)
+      end
+    end
+
+    ##
+    # A message is valid if it has a content.
+    #
+    # @return [Boolean]
+    def valid?
+      !!content
+    end
+
+    ##
+    # Returns the object itself as it is already a Pling::Message.
+    #
+    # @return [Pling::Message]
+    def to_pling_message
+      self
+    end
   end
 end
--- spec/pling/message_spec.rb
+++ spec/pling/message_spec.rb
@@ -2,4 +2,43 @@ require 'spec_helper'
 
 describe Pling::Message do
   
+  context 'when created with no arguments' do
+    it 'should not require an argument' do
+      expect { Pling::Message.new() }.to_not raise_error ArgumentError
+    end
+
+    specify { Pling::Message.new().should_not be_valid }
+  end
+
+  context 'when created with a string as first argument' do
+    subject { Pling::Message.new('Hello from Pling') }
+    its(:content) { should eq('Hello from Pling') }
+    it { should be_valid }
+  end
+
+  context 'when created with a hash that contains a :content key as first argument' do
+    subject { Pling::Message.new(:content => 'Hello from Pling') }
+    its(:content) { should eq('Hello from Pling') }
+    it { should be_valid }
+  end
+
+  context 'when created with an hash of invalid attributes' do
+    it 'should ignore the invalid paramters' do
+      expect { Pling::Message.new({ :random_param => true }) }.to_not raise_error
+    end
+  end
+
+  describe '#content=' do
+    it 'should call #to_s on the given content' do
+      subject.content = stub(:to_s => 'Hello from Pling')
+      subject.content.should eq('Hello from Pling')
+    end
+  end
+
+  describe '#to_pling_message' do
+    it 'should return self' do
+      subject.to_pling_message.should be === subject
+    end
+  end
+
 end
