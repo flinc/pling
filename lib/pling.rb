bf3092d97a8bdf157d9f844a64e1e0b4e756fc63
benedikt <benedikt@synatic.net>
2011-10-17 14:17:38 +0200

    Added Pling.configure and Pling.gateways

--- lib/pling.rb
+++ lib/pling.rb
@@ -1,5 +1,20 @@
 require "pling/version"
 
 module Pling
-  # Your code goes here...
+
+  @gateways = []
+
+  class << self
+    ##
+    # Stores the list of available gateway instances
+    #
+    # @return [Array] list of available gateways
+    attr_accessor :gateways
+
+    def configure
+      raise ArgumentError, 'No block given for Pling.configure' unless block_given?
+      yield self
+    end
+  end
+
 end
---
+++ spec/pling_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe Pling do
+  
+  subject { Pling }
+
+  it { should respond_to(:configure) }
+
+  describe '.configure' do
+    it 'should require a block' do
+      expect { subject.configure }.to raise_error(ArgumentError, /no block given/i)
+    end
+
+    it 'should call the block' do
+      expect { Pling.configure { throw :executed } }.to throw_symbol(:executed)
+    end
+
+    it 'should pass Pling to the block' do
+      Pling.configure do |config|
+        config.should be(Pling)
+      end
+    end
+  end
+
+  it { should respond_to(:gateways) }
+  it { should respond_to(:gateways=) }
+
+  describe '.gateways' do
+    it 'should default to an empty array' do
+      subject.gateways.should eq([])
+    end
+  end
+
+end
