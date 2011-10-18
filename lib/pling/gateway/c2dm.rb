c48c53d6a0e4775394f5b0a48f92a1329d437141
benedikt <benedikt@synatic.net>
2011-10-18 13:01:49 +0200

    Added authentication to Pling::Gateway::C2DM

--- lib/pling/gateway/c2dm.rb
+++ lib/pling/gateway/c2dm.rb
@@ -1,9 +1,14 @@
+require 'faraday'
+
 module Pling
   module Gateway
     class C2DM < Pling::Gateway::Base
 
+      attr_reader :token
+
       def initialize(configuration)
         setup_configuration(configuration, :require => [:email, :password, :source])
+        authenticate!
       end
 
       def deliver(message, device)
@@ -13,6 +18,42 @@ module Pling
         device  = device.to_pling_device
       end
 
+      private
+
+        def authenticate!
+          response = connection.post(configuration[:authentication_url], {
+            :accountType => 'HOSTED_OR_GOOGLE',
+            :service =>     'ac2dm',
+            :Email =>       configuration[:email],
+            :Passwd =>      configuration[:password],
+            :source =>      configuration[:source]
+          })
+
+          raise "C2DM Authentication failed: #{response.body}" unless response.success?
+
+          @token = extract_token(response.body)
+        end
+
+        def default_configuration
+          super.merge({
+            :authentication_url => 'https://www.google.com/accounts/ClientLogin',
+            :push_url => 'https://android.apis.google.com/c2dm/send',
+            :adapter => :net_http,
+            :connection => {}
+          })
+        end
+
+        def connection
+          @connection ||= Faraday.new(configuration[:authentication_url]) do |builder|
+            builder.use Faraday::Request::UrlEncoded
+            builder.adapter(configuration[:adapter])
+          end
+        end
+
+        def extract_token(body)
+          matches = body.match(/^Auth=(.+)$/)
+          matches && matches[1] or raise "C2DM Token extraction failed"
+        end
     end
   end
 end
--- spec/pling/gateway/c2dm_spec.rb
+++ spec/pling/gateway/c2dm_spec.rb
@@ -6,6 +6,16 @@ describe Pling::Gateway::C2DM do
     { :email => 'someone@gmail.com', :password => 'random', :source => 'some-source' }
   end
 
+  let(:response_mock) do
+    stub("Faraday response", :success? => true, :body => "SID=SOMESID\nAuth=S0ME-ToKeN123")
+  end
+
+  let(:connection_mock) do
+    stub("Faraday connection", :post => response_mock)
+  end
+
+  before { Faraday.stub(:new).and_return(connection_mock) }
+
   context 'when created with an invalid configuration' do
     [:email, :password, :source].each do |attribute|
       it 'should raise an error when #{attribute} is missing' do
@@ -20,6 +30,38 @@ describe Pling::Gateway::C2DM do
     it 'should not raise an error' do
       expect { Pling::Gateway::C2DM.new(valid_configuration) }.to_not raise_error
     end
+
+    it 'should try to authenticate' do
+      connection_mock.should_receive(:post)
+        .with('https://www.google.com/accounts/ClientLogin', {
+          :accountType => 'HOSTED_OR_GOOGLE',
+          :service => 'ac2dm',
+          :Email => valid_configuration[:email],
+          :Passwd => valid_configuration[:password],
+          :source => valid_configuration[:source]
+        })
+
+      Pling::Gateway::C2DM.new(valid_configuration)
+    end
+
+    it 'should extract the token from the response body' do
+      gateway = Pling::Gateway::C2DM.new(valid_configuration)
+      gateway.token.should eq("S0ME-ToKeN123")
+    end
+
+    it 'should raise an error if authentication was not successful' do
+      response_mock.stub(:success?).and_return(false)
+      response_mock.stub(:body).and_return("Error=BadAuthentication")
+
+      expect { Pling::Gateway::C2DM.new(valid_configuration) }.to raise_error(/Authentication failed: Error=BadAuthentication/)
+    end
+
+    it 'should raise an error if it could not extract a token from the response' do
+      response_mock.stub(:body).and_return("SOMERANDOMBODY")
+
+      expect { Pling::Gateway::C2DM.new(valid_configuration) }.to raise_error(/Token extraction failed/)
+    end
+
   end
 
   describe '#deliver' do
