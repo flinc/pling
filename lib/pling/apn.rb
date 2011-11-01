2152b388fe8ce135ef53eb2cacce448ac6e76e81
Konstantin Tennhard <me@t6d.de>
2011-11-01 15:05:42 +0100

    Adds a short description of the APN module

--- lib/pling/apn.rb
+++ lib/pling/apn.rb
@@ -1,4 +1,10 @@
 module Pling
+  
+  ##
+  # The {APN} module wraps Apple's Push Notification Service into an easy
+  # to use API. You can use instances of {Gateway} to send push notifications
+  # and instances of {Feedback} to get device identifiers that have been
+  # marked invalid.
   module APN
     autoload :Connection, 'pling/apn/connection'
     autoload :Gateway, 'pling/apn/gateway'
